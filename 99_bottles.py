a3bfce89fdb0a86287cc9349d770f315523b76c2
nhungbi <nhungvan16@gmail.com>
2022-05-23 18:08:13 -0500

    python fn

--- 99_bottles.py
+++ 99_bottles.py
@@ -1,19 +1,26 @@
-def bottle_song(bottle):
+def bottle_song(bottle = 99):
 	
-	#if bottle < 0:
-		#return ("Can't have negative bottle number.")
+    if bottle == 1:
+        end = """1 bottle of beer on the wall, 1 bottle of beer.
+Take one down and pass it around, no more bottles of beer on the wall.
+No more bottles of beer on the wall, no more bottles of beer.
+Go to the store and buy some more, 99 bottles of beer on the wall."""
+        return end
 
-    while bottle > 2:
-        yield f"{bottle} bottles of beer on the wall, {bottle} bottles of beer."
-        bottle -= 1
-        yield f"Take one down and pass it around, {bottle} bottles of beer on the wall."
+    current = f"{bottle} bottles of beer on the wall, {bottle} bottles of beer.\n" + f"Take one down and pass it around, {bottle-1} bottles of beer on the wall.\n"
+    return current + bottle_song(bottle-1)
 
-    end = """Take one down and pass it around, 1 bottle of beer on the wall.
-	1 bottle of beer on the wall, 1 bottle of beer.
-	Take one down and pass it around, no more bottles of beer on the wall.
-	No more bottles of beer on the wall, no more bottles of beer.
-	Go to the store and buy some more, 99 bottles of beer on the wall.
-	"""
-    yield end
+    #while bottle > 2:
+        #yield f"{bottle} bottles of beer on the wall, {bottle} bottles of beer."
+        #bottle -= 1
+        #yield f"Take one down and pass it around, {bottle} bottles of beer on the wall."
 
-print(list(bottle_song(10)))
+    #end = """Take one down and pass it around, 1 bottle of beer on the wall.
+	#1 bottle of beer on the wall, 1 bottle of beer.
+	#Take one down and pass it around, no more bottles of beer on the wall.
+	#No more bottles of beer on the wall, no more bottles of beer.
+	#Go to the store and buy some more, 99 bottles of beer on the wall.
+	# """
+    # yield end
+
+print(bottle_song(10))
